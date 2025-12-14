afcccd23b022ad2b8b5d04e78d37238cd65623f4
Aly <gitaly2433110@gmail.com>
2025-12-14 11:25:24 +0800

    Update schema_v3.sql

--- schema_v3.sql
+++ schema_v3.sql
@@ -1,11 +1,10 @@
-CREATE TABLE  (
-   int(11) NOT NULL AUTO_INCREMENT,
-   varchar(20) NOT NULL,
-   varchar(255) NOT NULL,
-   varchar(255) NOT NULL,
-   int(11) NOT NULL,
-  PRIMARY KEY (),
-  UNIQUE KEY  (),
-  UNIQUE KEY  ()
+CREATE TABLE `books` (
+  `book_id` int(11) NOT NULL AUTO_INCREMENT,
+  `book_number` varchar(20) NOT NULL,
+  `book_title` varchar(255) NOT NULL,
+  `book_author` varchar(255) NOT NULL,
+  `year_published` int(11) NOT NULL,
+  PRIMARY KEY (`book_id`),
+  UNIQUE KEY `book_number` (`book_number`),
+  UNIQUE KEY `book_title` (`book_title`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
