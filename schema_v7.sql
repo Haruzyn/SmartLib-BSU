65e0dbe8becbf7386b74d2745ce02569e9c6236c
Aly <gitaly2433110@gmail.com>
2025-12-14 11:28:26 +0800

    Update schema_v7.sql

--- schema_v7.sql
+++ schema_v7.sql
@@ -1,15 +1,15 @@
-CREATE TABLE  (
-   int(11) NOT NULL AUTO_INCREMENT,
-   int(11) NOT NULL,
-   int(11) DEFAULT NULL,
-   int(11) DEFAULT NULL,
-   text NOT NULL,
-   varchar(50) DEFAULT NULL,
-   datetime DEFAULT current_timestamp(),
-   tinyint(1) DEFAULT 0,
-  PRIMARY KEY (),
-  KEY  (),
-  KEY  (),
-  CONSTRAINT  FOREIGN KEY () REFERENCES  (),
-  CONSTRAINT  FOREIGN KEY () REFERENCES  ()
+CREATE TABLE `notifications` (
+  `notification_id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL,
+  `book_id` int(11) DEFAULT NULL,
+  `borrow_id` int(11) DEFAULT NULL,
+  `message` text NOT NULL,
+  `notification_type` varchar(50) DEFAULT NULL,
+  `date_sent` datetime DEFAULT current_timestamp(),
+  `is_read` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`notification_id`),
+  KEY `student_id` (`student_id`),
+  KEY `book_id` (`book_id`),
+  CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students_account` (`student_id`),
+  CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
