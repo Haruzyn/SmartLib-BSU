c078a96d50c5f69cc6c703d94b8414cd7467e9b8
Aly <gitaly2433110@gmail.com>
2025-12-17 00:57:53 +0800

    Update schema_v5.sql

--- schema_v5.sql
+++ schema_v5.sql
@@ -1,13 +1,13 @@
-CREATE TABLE `borrowed_book` (
-  `borrow_id` int(11) NOT NULL AUTO_INCREMENT,
-  `student_id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL,
-  `date_borrowed` date NOT NULL,
-  `due_date` date NOT NULL,
-  `date_returned` date DEFAULT NULL,
-  PRIMARY KEY (`borrow_id`),
-  KEY `student_id` (`student_id`),
-  KEY `book_id` (`book_id`),
-  CONSTRAINT `borrowed_book_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students_account` (`student_id`),
-  CONSTRAINT `borrowed_book_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE borrowed_book (
+  borrow_id int(11) NOT NULL AUTO_INCREMENT,
+  student_id int(11) NOT NULL,
+  book_id int(11) NOT NULL,
+  date_borrowed date NOT NULL,
+  due_date date NOT NULL,
+  date_returned date DEFAULT NULL,
+  PRIMARY KEY (borrow_id),
+  KEY student_id (student_id),
+  KEY book_id (book_id),
+  CONSTRAINT borrowed_book_ibfk_1 FOREIGN KEY (student_id) REFERENCES students_account (student_id),
+  CONSTRAINT borrowed_book_ibfk_2 FOREIGN KEY (book_id) REFERENCES books (book_id)
+);
