1e850c9bb6db3aaa0eb5714615c6c685a66ac8dc
Aly <gitaly2433110@gmail.com>
2025-12-17 00:52:22 +0800

    Update schema_v1.sql

--- schema_v1.sql
+++ schema_v1.sql
@@ -1,6 +1,6 @@
-CREATE TABLE `admin_account` (
-  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
-  `admin_email` varchar(50) NOT NULL UNIQUE,
-  `admin_password` varchar(50) NOT NULL UNIQUE,
-  PRIMARY KEY (`admin_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE admin_account (
+  admin_id int(11) NOT NULL AUTO_INCREMENT,
+  admin_email varchar(50) NOT NULL UNIQUE,
+  admin_password varchar(50) NOT NULL UNIQUE,
+  PRIMARY KEY (admin_id)
+);
