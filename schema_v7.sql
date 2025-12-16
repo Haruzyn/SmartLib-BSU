446b45d8e293ec67d42d5ea8253dd8c49ee972ee
Aly <gitaly2433110@gmail.com>
2025-12-17 01:01:22 +0800

    Update schema_v7.sql

--- schema_v7.sql
+++ schema_v7.sql
@@ -1,15 +1,15 @@
-CREATE TABLE `notifications` (
-  `notification_id` int(11) NOT NULL AUTO_INCREMENT,
-  `student_id` int(11) NOT NULL,
-  `book_id` int(11) DEFAULT NULL,
-  `borrow_id` int(11) DEFAULT NULL,
-  `message` text NOT NULL,
-  `notification_type` varchar(50) DEFAULT NULL,
-  `date_sent` datetime DEFAULT current_timestamp(),
-  `is_read` tinyint(1) DEFAULT 0,
-  PRIMARY KEY (`notification_id`),
-  KEY `student_id` (`student_id`),
-  KEY `book_id` (`book_id`),
-  CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students_account` (`student_id`),
-  CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE notifications (
+  notification_id int(11) NOT NULL AUTO_INCREMENT,
+  student_id int(11) NOT NULL,
+  book_id int(11) DEFAULT NULL,
+  borrow_id int(11) DEFAULT NULL,
+  message text NOT NULL,
+  notification_type varchar(50) DEFAULT NULL,
+  date_sent datetime DEFAULT current_timestamp(),
+  is_read tinyint(1) DEFAULT 0,
+  PRIMARY KEY (notification_id),
+  KEY student_id (student_id),
+  KEY book_id (book_id),
+  CONSTRAINT notifications_ibfk_1 FOREIGN KEY (student_id) REFERENCES students_account (student_id),
+  CONSTRAINT notifications_ibfk_2 FOREIGN KEY (book_id) REFERENCES books (book_id)
+);
