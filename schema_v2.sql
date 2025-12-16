f1196cd203e3bb52fdce40aa5940733a0914cc15
Aly <gitaly2433110@gmail.com>
2025-12-17 00:53:19 +0800

    Update schema_v2.sql

--- schema_v2.sql
+++ schema_v2.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `students_account` (
-  `student_id` int(11) NOT NULL AUTO_INCREMENT,
-  `student_name` varchar(60) NOT NULL,
-  `g_suite` varchar(50) NOT NULL,
-  `sr_code` varchar(50) NOT NULL,
-  `program` varchar(50) NOT NULL,
-  PRIMARY KEY (`student_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE students_account (
+  student_id int(11) NOT NULL AUTO_INCREMENT,
+  student_name varchar(60) NOT NULL UNIQUE,
+  g_suite varchar(50) NOT NULL UNIQUE,
+  sr_code varchar(50) NOT NULL UNIQUE,
+  program varchar(50) NOT NULL,
+  PRIMARY KEY (student_id)
+);
